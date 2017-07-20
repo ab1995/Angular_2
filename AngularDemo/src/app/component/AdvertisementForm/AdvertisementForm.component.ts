a693c3a4773bfb1e880d1f461f51e58f9238b674
Gaurav Dhus <Dhus_g@India.XoriantCorp.com>
2017-07-20 17:58:33 +0530

    assign updated

--- AngularDemo/src/app/component/AdvertisementForm/AdvertisementForm.component.ts
+++ AngularDemo/src/app/component/AdvertisementForm/AdvertisementForm.component.ts
@@ -1,5 +1,4 @@
 import { Component,NgModule } from '@angular/core';
-import { NgFor } from '@angular/common';
 
 @Component({
     selector:"myForm",
@@ -7,29 +6,36 @@ import { NgFor } from '@angular/common';
                 <label>Name</label>
                 <input type="text" [value]="defaultName" #inputName><br>
                 <label>Category</label>
-                <select #inputcategory>
-                    <option>Furniture</option>
-                    <option>Hardware</option>
-                    <option>Mobile</option>
+                <select  #inputCategory>
+                    <option *ngFor="let item of items; let i=index" >{{item}}</option>
                 </select> <br>
                 <label>Description</label><br><textarea rows="4" #inputDescription></textarea><br>
-                <button type="Submit" (click)="postAdvertisement(inputName.value, inputcategory.value,inputDescription.value)">Post</button>`
+                <button type="Submit" (click)="postAdvertisement(inputName.value,inputCategory.value,inputDescription.value)">Post</button>
+                <br><br>
+                    <table>
+        <thead>
+            <th>Name</th>
+        </thead>
+        <tbody>
+            <tr *ngFor="let hero of heroes; trackBy: trackHero" >
+                <td>{{hero.name}}</td>
+            </tr>
+        </tbody>
+    </table>`
 })
 
 export class AdvertisementForm{
     defaultName:string="xyz";
-    title:String="Post Ad"
-    categoryList = [
-    {id: 1, name:'Furniture'},
-    {id: 2, name:'Hardware'},
-    {id: 3, name:'Mobile'}
-    ];
+    title:String="Post Ad";
+    
+    items:String[];
+    constructor(){
+        this.items=['Furniture', 'Hardware', 'Mobile' ];
+    }
 
-    advertisements:any=[];
-
-    postAdvertisement()
+    postAdvertisement(inputName:String, inputcategory:String,inputDescription:String)
     {
-
+        console.log(inputName, inputcategory, inputDescription );
     }
 
 
